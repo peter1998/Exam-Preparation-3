910eebe86a6d7963f97d2290fc27e79133123f82
Peter Matov <pmatov@gmail.com>
2018-04-05 15:38:34 +0300

    added third exercise from the third exam

---
+++ RageQuit.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Exceptions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using System.Text;
+
+
+    public class Exceptions
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToUpper();
+            StringBuilder sb = new StringBuilder();
+            Regex regex = new Regex(@"(\D+)(\d+)");
+            Match match = regex.Match(input);
+
+            do
+            {
+                string stringToAppend = match.Groups[1].Value;
+                int counter = int.Parse(match.Groups[2].Value);
+
+                for (int i = 0; i < counter; i++)
+                {
+                    sb.Append(stringToAppend);
+                }
+                match = match.NextMatch();
+            } while (match.Success);
+
+            int count = sb.ToString().Distinct().Count();
+            Console.WriteLine($"Unique symbols used: {count}");
+            Console.WriteLine(sb);
+        }
+    }
+}
