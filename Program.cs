a110230efbd588e17f707c159844fc90a0c5fe38
Peter Matov <pmatov@gmail.com>
2018-04-13 11:59:59 +0300

    Adding new List<KeyValuePair<string,int>>();

--- Program.cs
+++ Program.cs
@@ -1,146 +1,53 @@
-﻿
-namespace Problem_2___Command_Interpreter
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_Evolution
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
-
-
-
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main()
         {
-                   var list = Console.ReadLine().Split().ToList();
-            string[] commands = new string[5];
-            string input = string.Empty;
-            int start = 0;
-            int count = 0;
-            while (true)
+            var pokemon = new Dictionary<string, List<KeyValuePair<string, int>>>();
+            string input = Console.ReadLine();
+            while (input != "wubbalubbadubdub")
             {
-                try
+                string[] inputs = input.Split(new char[] { '-', '>', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (inputs.Length == 1)
                 {
-                    commands = Console.ReadLine().Split(new char[] {' '} , StringSplitOptions.RemoveEmptyEntries);
-                    input = commands[0];
-                    if (input == "end")
+                    string name = inputs[0];
+                    if (pokemon.ContainsKey(name))
                     {
-                       
-
-                        Console.WriteLine($"{'['}{string.Join(", ", list)}{']'}");
-                        return;
-                    }
-                    else
-                    {
-                        switch (input)
+                        Console.WriteLine("# {0}", name);
+                        foreach (var kvp in pokemon[name])
                         {
-                            case "reverse":
-                                start = int.Parse(commands[2]);
-                                count = int.Parse(commands[4]);
-
-                                if (IsValid(list,start,count))
-                                {
-                                    list.Reverse(start, count);
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Invalid input parameters.");
-                                }
-
-
-                                break;
-
-                            case "rollRight":
-                                count = int.Parse(commands[1]);
-                                if (IsValid(list, start, count))
-                                {
-                                    rollRight(list, count);
-                                }
-                                
-                                break;
-                            case "rollLeft":
-                                count = int.Parse(commands[1]);
-                                if (IsValid(list, start, count))
-                                {
-                                    rollLeft(list, count);
-                                }
-                                
-                                break;
-                            case "sort":
-                                start = int.Parse(commands[2]);
-                                count = int.Parse(commands[4]);
-                                if (IsValid(list, start, count))
-                                {
-                                    Sort(list,start, count);
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Invalid input parameters.");
-                                }
-
-                               
-                                break;
-                            default:
-                                break;
+                            Console.WriteLine("{0} <-> {1}", kvp.Key, kvp.Value);
                         }
-                       
-                    }                }
-                catch (IndexOutOfRangeException )
+                    }
+                }
+                else
                 {
-
-                    Console.WriteLine("Invalid input parameters.");
+                    string name = inputs[0];
+                    string evolution = inputs[1];
+                    int evoIndex = int.Parse(inputs[2]);
+                    if (!pokemon.ContainsKey(name))
+                    {
+                        pokemon.Add(name, new List<KeyValuePair<string, int>>());
+                    }
+                    pokemon[name].Add(new KeyValuePair<string, int>(evolution, evoIndex));
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var name in pokemon)
+            {
+                Console.WriteLine("# {0}", name.Key);
+                foreach (var kvp in name.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine("{0} <-> {1}", kvp.Key, kvp.Value);
                 }
             }
-
-            
         }
-
-        private static void Sort(List<string> list, int start, int count)
-        {
-            var resultarray = list.Skip(start).Take(count).OrderBy(x => x).ToList();
-            list.RemoveRange(start, count);
-            list.InsertRange(start, resultarray);
-        }
-
-        private static void rollLeft(List<string> list, int count)
-        {
-            count = count % list.Count();
-            for (int i = 0; i < count; i++)
-            {
-                string firstElem = list[0];
-                for (int j = 0; j < list.Count-1; j++)
-                {
-                    list[j] = list[j + 1];
-                }
-
-                list[list.Count - 1] = firstElem;
-            }
-           
-        }
-
-        private static bool IsValid(List<string> arr, int start, int count)
-        {
-            if (start>=0 && start<arr.Count && count>0 && (start + count)<=arr.Count)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        private static void rollRight(List<string> arr, int count)
-        {
-            count = count % arr.Count();
-            for (int i = 0; i < count; i++)
-            {
-                string lastElem = arr.Last();
-                arr.RemoveAt(arr.Count - 1);
-                arr.Insert(0, lastElem);
-            }
-
-        }
-
-      
     }
 }
